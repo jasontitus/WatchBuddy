c249996843be2bbd4abdaab0cb6bdb142b5b9542
Jason Titus <jason.titus@gmail.com>
2026-02-18 19:34:49 -0800

    Initial Commit

---
+++ WatchAI/WatchAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAIApp.swift
+//  WatchAI
+//
+//  Created by Jason Titus on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
