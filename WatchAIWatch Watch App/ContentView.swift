558a75abe178bcb5d231461aed9534f0f17bcae7
Jason Titus <jason.titus@gmail.com>
2026-02-21 14:51:08 -0800

    Add privacy policy and support pages

---
+++ WatchAIWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchAIWatch Watch App
+//
+//  Created by Jason Titus on 2/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchAIWatch Watch App/WatchAIWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAIWatchApp.swift
+//  WatchAIWatch Watch App
+//
+//  Created by Jason Titus on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchAIWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
